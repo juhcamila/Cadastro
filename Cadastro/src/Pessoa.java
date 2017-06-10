029c349ed397ebd101b5dabef49862df3375e9e7
LeoFalco <leonardo.falco@outlook.com>
2017-06-10 12:35:39 -0300

    Projeto novo

---
+++ Cadastro/src/Pessoa.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author MAQLABiii
+ */
+public class Pessoa {
+    private int id;
+    private String nome;
+
+    public Pessoa() {
+    }
+    
+    public Pessoa(int id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    
+}
